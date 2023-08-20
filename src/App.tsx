1f09346f9d06c4ef73431a27a0b4f38d2ca80e1e
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-20 14:21:33 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello {user?.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,74 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify, Auth } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+Amplify.configure({
+  Auth: {
+
+    // REQUIRED - Amazon Cognito Region
+    region: 'us-east-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'us-east-1_8Ftjz9EEF',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '6gda8mjiu3nph79p5f84vb7nnv',
+
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: false,
+
+    // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+    // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+    signUpVerificationMethod: 'code', // 'code' | 'link'
+
+    // // OPTIONAL - Configuration for cookie storage
+    // // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+    // cookieStorage: {
+    //   // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+    //   domain: '.yourdomain.com',
+    //   // OPTIONAL - Cookie path
+    //   path: '/',
+    //   // OPTIONAL - Cookie expiration in days
+    //   expires: 365,
+    //   // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+    //   sameSite: 'strict' | 'lax',
+    //   // OPTIONAL - Cookie secure flag
+    //   // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+    //   secure: true,
+    // },
+
+    // OPTIONAL - customized storage object
+    // storage: MyStorage,
+
+    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+    // authenticationFlowType: 'USER_PASSWORD_AUTH',
+
+    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+    clientMetadata: {myCustomKey: 'myCustomValue'},
+
+    // OPTIONAL - Hosted UI configuration
+    oauth: {
+      domain: 'your_cognito_domain',
+      scope: [
+        'phone',
+        'email',
+        'profile',
+        'openid',
+        'aws.cognito.signin.user.admin',
+      ],
+      redirectSignIn: 'http://localhost:3000/',
+      redirectSignOut: 'http://localhost:3000/',
+      responseType: 'code', // or 'token', note that REFRESH token will only be generated when the responseType is code
+    },
+  },
+});
+
+
+// You can get the current config object
+const currentConfig = Auth.configure();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
