583f85959efe91f18cdc5401dccbbedfd9c52f5f
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-21 14:14:57 +0300

    feat: Add Toggle Button to SearchBar

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import awsconfig from './aws-exports';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
+import { Container, Box } from '@mui/material';
 Amplify.configure(awsconfig);
 
 
@@ -13,13 +14,19 @@ Amplify.configure(awsconfig);
 function App({ signOut, user }: WithAuthenticatorProps) {
   return (
     <Router>
-      <div>
-        <NavBar signOut={signOut} user={user} />
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <div>
+          <NavBar signOut={signOut} user={user} />
+        </div>
+        <Container sx={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+        <Box component="footer" sx={{ textAlign: 'center', py: 2 }}>
+          Calo Nutrition Calculator
+        </Box>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      
     </Router>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,22 +1,37 @@
 import { Box, Container, InputAdornment, TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function SearchBar() {
+interface SearchBarProps {
+  selectedValue: string;
+}
+
+export default function SearchBar({ selectedValue }: SearchBarProps) {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
+  let placeholderText = "Search Ingredient";
+
+  if (selectedValue === "ingredient") {
+    placeholderText = "Search Ingredients";
+  } else if (selectedValue === "component") {
+    placeholderText = "Search Components";
+  } else if (selectedValue === "meal") {
+    placeholderText = "Search Meals";
+  }
+
   return (
     <Container maxWidth="md">
-      <Box sx={{ display: "flex", justifyContent: "center", mt: 20 }}>
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
         <TextField
           id="search"
-          label="Search"
+          label={searchTerm}
           value={searchTerm}
           onChange={handleChange}
+          placeholder={placeholderText}
           sx={{
             "& .MuiOutlinedInput-root": {
               borderRadius: "20px",
