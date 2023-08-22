53329888b0a924280dff8b8e76cbc2aa238c0768
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-22 13:03:51 +0300

    feat: Add image to home page

--- src/network/ingredient_api.ts
+++ src/network/ingredient_api.ts
@@ -2,8 +2,8 @@ import { Ingredient } from "../models/ingredient";
 import {fetchData} from "./base_api"
 
 // Set the base URL for the backend API
-// const API_BASE_URL = 'https://bdy09cap8a.execute-api.us-east-1.amazonaws.com/dev/v1/ingredients';
-const API_BASE_URL = 'http://localhost:3000/dev/v1/ingredients';
+const API_BASE_URL = 'https://bdy09cap8a.execute-api.us-east-1.amazonaws.com/dev/v1/ingredients';
+// const API_BASE_URL = 'http://localhost:3000/dev/v1/ingredients';
 
 
 // Function to fetch ingredients from the backend API
