490fbb0648bda76d3b110aef254637900514d306
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-22 12:25:36 +0300

    feat: Add ingredients to table
    -use fetch api to get data
    -display data in ingredients table

--- src/network/ingredient_api.ts
+++ src/network/ingredient_api.ts
@@ -1,21 +1,14 @@
 import { Ingredient } from "../models/ingredient";
+import {fetchData} from "./base_api"
 
 // Set the base URL for the backend API
-// const API_BASE_URL = 'https://bdy09cap8a.execute-api.us-east-1.amazonaws.com/dev/';
+// const API_BASE_URL = 'https://bdy09cap8a.execute-api.us-east-1.amazonaws.com/dev/v1/ingredients';
+const API_BASE_URL = 'http://localhost:3000/dev/v1/ingredients';
 
-async function fetchData(input: RequestInfo, init?: RequestInit) {
-    const response = await fetch(input, init);
-    if (response.ok) {
-      return response;
-    } else {
-      const errorBody = await response.json();
-      const errorMessage = errorBody.error;
-      throw Error(errorMessage);
-    }
-  }
 
 // Function to fetch ingredients from the backend API
 export async function fetchIngredients(): Promise<Ingredient[]> {
-  const response = await fetchData('/dev/v1/ingredients', { method: 'GET' });
-  return response.json();
+  const response = await fetchData(API_BASE_URL, { method: 'GET' });
+  console.log(response.data)
+  return response.data;
 }
