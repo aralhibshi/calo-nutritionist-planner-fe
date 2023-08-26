ba3cb77133904d4c3552b908a553c0cdba2c4296
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-26 14:20:27 +0300

    Feat: Use formik in addingredient form

--- src/pages/ingredient/AddIngredientDialog.tsx
+++ src/pages/ingredient/AddIngredientDialog.tsx
@@ -7,6 +7,7 @@ import DialogActions from '@mui/material/DialogActions';
 import TextField from '@mui/material/TextField';
 import * as IngredientsApi from '../../network/ingredientApi';
 import { ICreateIngredientInput } from '../../interfaces/ingredient';
+import { useFormik } from 'formik';
 
 interface AddIngredientDialogProps {
   onIngredientAdded: (newIngredient: ICreateIngredientInput) => void;
@@ -15,134 +16,129 @@ interface AddIngredientDialogProps {
 export default function AddIngredientDialog({ onIngredientAdded }: AddIngredientDialogProps) {
   const [open, setOpen] = useState(false);
 
-  const [formData, setFormData] = useState<ICreateIngredientInput>({
-    name: '',
-    category: '',
-    description: '',
-    price: 0,
-    protein: 0,
-    fats: 0,
-    carbs: 0,
-    unit: '',
-  });
-
-  const openFormDialog = () => {
-    setOpen(true);
-  };
-
   const closeFormDialog = () => {
     setOpen(false);
   };
-
-  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-
-  const handleFormSubmit = async () => {
-    try {
-      console.log('Form data:', formData);
   
-      const newIngredient = await IngredientsApi.createIngredient(formData);
-      console.log('New ingredient:', newIngredient);
-  
-      onIngredientAdded(newIngredient);
-      closeFormDialog();
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      category: '',
+      description: '',
+      price: 0,
+      protein: 0,
+      fats: 0,
+      carbs: 0,
+      unit: '',
+    },
+    onSubmit: async (values) => {
+      try {
+        console.log('Form data:', values);
 
-    } catch (error) {
-      console.log('Error:', error);
-      alert(error);
-    }
+        const newIngredient = await IngredientsApi.createIngredient(values);
+        console.log('New ingredient:', newIngredient);
+
+        onIngredientAdded(newIngredient);
+        closeFormDialog();
+      } catch (error) {
+        console.log('Error:', error);
+        alert(error);
+      }
+    },
+  });
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    formik.handleSubmit(e);
   };
 
   return (
     <>
-    <div  style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
-      <Button variant="contained" color='primary' onClick={openFormDialog}>
-        Add Ingredient
-      </Button>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
+        <Button variant="contained" color='primary' onClick={() => setOpen(true)}>
+          Add Ingredient
+        </Button>
       </div>
       <Dialog open={open} onClose={closeFormDialog}>
         <DialogTitle>Add Ingredient</DialogTitle>
         <DialogContent>
-          <TextField
-            label="Name"
-            name="name"
-            value={formData.name}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Category"
-            name="category"
-            value={formData.category}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Description"
-            name="description"
-            value={formData.description}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Price"
-            name="price"
-            type="number"
-            value={formData.price}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Protein"
-            name="protein"
-            type="number"
-            value={formData.protein}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Fats"
-            name="fats"
-            type="number"
-            value={formData.fats}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Carbs"
-            name="carbs"
-            type="number"
-            value={formData.carbs}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Unit"
-            name="unit"
-            value={formData.unit}
-            onChange={handleFormChange}
-            fullWidth
-            margin="dense"
-          />
+          <form onSubmit={handleFormSubmit}>
+            <TextField
+              label="Name"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Category"
+              name="category"
+              value={formik.values.category}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Description"
+              name="description"
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Price"
+              name="price"
+              type="number"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Protein"
+              name="protein"
+              type="number"
+              value={formik.values.protein}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Fats"
+              name="fats"
+              type="number"
+              value={formik.values.fats}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Carbs"
+              name="carbs"
+              type="number"
+              value={formik.values.carbs}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Unit"
+              name="unit"
+              value={formik.values.unit}
+              onChange={formik.handleChange}
+              fullWidth
+              margin="dense"
+            />
+            <DialogActions>
+              <Button onClick={closeFormDialog}>Cancel</Button>
+              <Button variant="contained" type="submit">
+                Add
+              </Button>
+            </DialogActions>
+          </form>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={closeFormDialog}>Cancel</Button>
-          <Button variant="contained" onClick={handleFormSubmit}>
-            Add
-          </Button>
-        </DialogActions>
       </Dialog>
     </>
   );
