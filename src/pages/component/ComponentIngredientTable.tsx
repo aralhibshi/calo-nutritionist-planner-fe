0a5a91ad252af3ed38e1f33bb0055f30e6899a38
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-08-31 23:07:48 +0300

    feat: componentIngredientTable scrolling

--- src/pages/component/ComponentIngredientTable.tsx
+++ src/pages/component/ComponentIngredientTable.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Table from "@mui/joy/Table";
 import CircularProgress from "@mui/material/CircularProgress";
-import { Backdrop, IconButton } from "@mui/material";
+import { Backdrop } from "@mui/material";
 import * as IngredientsApi from "../../network/ingredientApi";
 import useIngredientStore from "../../stores/ingredientStore";
 import useSearchStore from "../../stores/searchStore";
@@ -31,7 +31,7 @@ const IngredientTable: React.FC = () => {
     try {
       setComponentLoading(true);
       const take = 9;
-      const response = await IngredientsApi.fetchIngredients(skip,take);
+      const response = await IngredientsApi.fetchIngredients(0, 100);
       setEntityCount(response.count);
       setComponentSearchResult(response.data)
       if (componentSearchResult) {
@@ -59,63 +59,63 @@ const IngredientTable: React.FC = () => {
           }}
           open={true}
         >
-          <CircularProgress
-            color="inherit"
-          />
+          <CircularProgress color="inherit" />
         </Backdrop>
       )}
-      <Table
-        hoverRow
-        sx={{
-          marginTop: "15px",
-          userSelect: "none",
-        }}
-        id='table'
-      >
-        <thead>
-          <tr
-          >
-            <th style={{ width: "40%" }}>Ingredient Name&nbsp;</th>
-            <th>Calories&nbsp;</th>
-            <th>Protein&nbsp;</th>
-            <th>Carbs&nbsp;</th>
-            <th>Fat&nbsp;</th>
-            <th>Unit&nbsp;</th>
-            <th>Price&nbsp;(BHD)</th>
-          </tr>
-        </thead>
-        <tbody>
-        {componentSearchResult && Array.isArray(componentSearchResult) && componentSearchResult.length > 0 ? (
-          componentSearchResult.map((ingredient: IIngredient, index: number) => {
-            const calories: string = (
-              ingredient.fats * 9 +
-              ingredient.carbs * 4 +
-              ingredient.protein * 4
-            )
-              .toFixed(3)
-              .padEnd(5, "0");
-            return (
-              <tr
-                id='table'
-                key={index}
-              >
-                <td>{ingredient.name}</td>
-                <td>{calories}</td>
-                <td>{ingredient.protein}</td>
-                <td>{ingredient.carbs}</td>
-                <td>{ingredient.fats}</td>
-                <td>{ingredient.unit}</td>
-                <td>{ingredient.price}</td>
-              </tr>
-            );
-          })
-          ) : (
+      <div style={{ overflowY: "auto", maxHeight: "400px" }}>
+        <Table
+          hoverRow
+          sx={{
+            marginTop: "15px",
+            userSelect: "none",
+          }}
+          id="table"
+        >
+          <thead>
             <tr>
-              <td colSpan={8}>No search results found.</td>
+              <th style={{ width: "40%" }}>Ingredient Name&nbsp;</th>
+              {/* <th>Calories&nbsp;</th>
+              <th>Protein&nbsp;</th>
+              <th>Carbs&nbsp;</th>
+              <th>Fat&nbsp;</th>
+              <th>Unit&nbsp;</th> */}
+              <th>Price&nbsp;(BHD)</th>
             </tr>
-          )}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {componentSearchResult &&
+            Array.isArray(componentSearchResult) &&
+            componentSearchResult.length > 0 ? (
+              componentSearchResult.map(
+                (ingredient: IIngredient, index: number) => {
+                  const calories: string = (
+                    ingredient.fats * 9 +
+                    ingredient.carbs * 4 +
+                    ingredient.protein * 4
+                  )
+                    .toFixed(3)
+                    .padEnd(5, "0");
+                  return (
+                    <tr key={index}>
+                      <td>{ingredient.name}</td>
+                      {/* <td>{calories}</td>
+                      <td>{ingredient.protein}</td>
+                      <td>{ingredient.carbs}</td>
+                      <td>{ingredient.fats}</td>
+                      <td>{ingredient.unit}</td> */}
+                      <td>{ingredient.price}</td>
+                    </tr>
+                  );
+                }
+              )
+            ) : (
+              <tr>
+                <td colSpan={8}>No search results found.</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </div>
     </>
   );
 };
