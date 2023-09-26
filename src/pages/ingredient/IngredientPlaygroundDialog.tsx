362ebfb8d3b7ac17940edd670d426aa91d5b9288
aralhibshi <118065642+aralhibshi@users.noreply.github.com>
2023-09-26 13:26:45 +0300

    feat: Playground Dialog Title & Indentation

--- src/pages/ingredient/IngredientPlaygroundDialog.tsx
+++ src/pages/ingredient/IngredientPlaygroundDialog.tsx
@@ -9,9 +9,8 @@ import Button from '@mui/material/Button';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import IngredientComponentTable from './IngredientComponentTable';
 import { IIngredientData } from '../../interfaces';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import IngredientMealTable from './IngredientMealTable';
-import theme from '../../theme/theme';
+import { useTheme } from '@mui/material/styles';
 
 interface IngredientPlaygroundDialogProps {
   open: boolean;
@@ -19,6 +18,7 @@ interface IngredientPlaygroundDialogProps {
 }
 
 const IngredientPlaygroundDialog: React.FC<IngredientPlaygroundDialogProps> = (props) => {
+  const theme = useTheme();
 
   const handleClose = () => {
     props.setOpen(false);
@@ -26,58 +26,174 @@ const IngredientPlaygroundDialog: React.FC<IngredientPlaygroundDialogProps> = (p
 
   return (
     <Dialog
-    open={props.open}
-    onClose={handleClose}
-    fullScreen
+      open={props.open}
+      onClose={handleClose}
+      fullScreen
     >
-      <Card style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <CardContent style={{ flex: 1 }}>
-          <Grid container spacing={2}>
-            <Grid item container xs={2} spacing={2} alignItems="center">
-              <Grid item xs={12}>
-                <Typography variant="h6">Section 1</Typography>
+      <Card
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '8vh',
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.primary.contrastText
+        }}
+      >
+        <Typography
+          variant='h5'
+          align='center'
+          style={{
+            margin: '16px 0'
+          }}
+        >
+          Ingredient Playground
+        </Typography>
+      </Card>
+      <Card
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '92vh'
+        }}
+      >
+        <CardContent
+          style={{
+            flex: 1
+          }}
+        >
+          <Grid
+            container
+            spacing={2}
+          >
+            <Grid
+              item
+              container
+              xs={2}
+              spacing={2}
+              alignItems='center'
+            >
+              <Grid
+                item 
+                xs={12}
+              >
+                <Typography
+                  variant='h6'
+                >
+                  Section 1
+                </Typography>
               </Grid>
               {/* Add your content for Section 1 here */}
-              <Grid item xs={12}>
-                <TextField variant="outlined" label="Price" fullWidth />
+              <Grid
+                item
+                xs={12}
+              >
+                <TextField
+                  variant='outlined'
+                  label='Price'
+                  fullWidth
+                />
               </Grid>
-              <Grid item xs={12}>
-                <TextField variant="outlined" label="Protein" fullWidth />
+              <Grid
+                item
+                xs={12}
+              >
+                <TextField
+                  variant='outlined'
+                  label='Protein'
+                  fullWidth
+                />
               </Grid>
-              <Grid item xs={12}>
-                <TextField variant="outlined" label="Carbs" fullWidth />
+              <Grid
+                item xs={12}>
+                <TextField
+                  variant='outlined'
+                  label='Carbs'
+                  fullWidth
+                />
               </Grid>
-              <Grid item xs={12}>
-                <TextField variant="outlined" label="Fats" fullWidth />
+              <Grid
+                item
+                xs={12}
+              >
+                <TextField
+                  variant='outlined'
+                  label='Fats'
+                  fullWidth
+                />
               </Grid>
             </Grid>
-            <Grid item container xs={8} spacing={2} alignItems="center">
-              <Grid item xs={12}>
-                <Typography variant="h6">Section 2</Typography>
+            <Grid
+              item
+              container
+              xs={8}
+              spacing={2}
+              alignItems='center'
+            >
+              <Grid
+                item
+                xs={12}
+              >
+                <Typography
+                  variant='h6'
+                >
+                  Section 2
+                </Typography>
               </Grid>
               {/* Add your content for Section 2 here */}
-              <Grid item xs={12}>
+              <Grid
+                item xs={12}
+              >
+
                 <IngredientComponentTable />
+
               </Grid>
-              <Grid item xs={12}>
-              <Grid item xs={12}>
+              <Grid
+                item xs={12}
+              >
+              <Grid
+              item xs={12}
+              >
                 {/* <ArrowDownwardIcon /> */}
               </Grid>
               </Grid>
-              <Grid item xs={12}>
+              <Grid
+                item xs={12}
+              >
+
               <IngredientMealTable/>
+
               </Grid>
 
             </Grid>
-            <Grid item container xs={2} spacing={2} alignItems="center">
-              <Grid item xs={12}>
-                <Typography variant="h6">Section 3</Typography>
+            <Grid
+              item
+              container
+              xs={2}
+              spacing={2}
+              alignItems='center'
+            >
+              <Grid
+                item
+                xs={12}
+              >
+                <Typography
+                  variant='h6'
+                >
+                  Section 3
+                </Typography>
               </Grid>
               {/* Add your content for Section 3 here */}
             </Grid>
           </Grid>
         </CardContent>
-        <Button variant="contained" onClick={handleClose} style={{ alignSelf: 'flex-end', margin: '16px' }}>
+        <Button
+          variant='contained'
+          onClick={handleClose}
+          style={{
+            alignSelf: 'flex-end',
+            margin: '16px'
+          }}
+        >
           Close
         </Button>
       </Card>
