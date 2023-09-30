3664d2191392d5752fad2ee817cf5c59764d5163
kidito-ken <95145835+kidito-ken@users.noreply.github.com>
2023-09-30 16:45:44 +0300

    fix: mealTable calculation

--- src/pages/meal/MealTable.tsx
+++ src/pages/meal/MealTable.tsx
@@ -149,25 +149,33 @@ const MealTable: React.FC = () => {
               let totalProteins = 0;
               let totalCalories = 0;
               let totalPrice = 0;
+              let totalQuantity = 0;
 
               if (
                 meal.meals_components &&
                 Array.isArray(meal.meals_components)
               ) {
                 meal.meals_components?.map((el: IMealComponent) => {
+
                   const quantity = Number(el.component_quantity);
+
                   el.component.components_ingredients?.map(
                     (el: IComponentIngredient) => {
-                      const ingredientQuantity = Number(
-                        el.ingredient_quantity
-                      );
-                      totalFats += Number(el.ingredient.fats * ingredientQuantity);
-                      totalCarbs += Number(el.ingredient.carbs * ingredientQuantity);
-                      totalProteins += Number(el.ingredient.protein * ingredientQuantity);
-                      totalPrice += Number(el.ingredient.price * ingredientQuantity);
+
+                      totalFats += Number(el.ingredient.fats * el.ingredient_quantity);
+                      totalCarbs += Number(el.ingredient.carbs * el.ingredient_quantity);
+                      totalProteins += Number(el.ingredient.protein * el.ingredient_quantity);
+                      totalPrice += Number(el.ingredient.price * el.ingredient_quantity);
+
+                      totalQuantity += Number(el.ingredient_quantity);
 
                     }
                   );
+
+                  totalProteins /= totalQuantity
+                  totalCarbs /= totalQuantity
+                  totalFats /= totalQuantity
+                  totalPrice /= totalQuantity
 
                   totalProteins += Number(
                     (totalProteins * quantity).toFixed(3)
@@ -176,8 +184,8 @@ const MealTable: React.FC = () => {
                   totalFats += Number((totalFats * quantity).toFixed(3));
                   totalPrice += Number((totalPrice * quantity).toFixed(3));
 
-                  totalCalories +=
-                  totalFats * 9 + totalCarbs * 4 + totalProteins * 4;
+                  totalCalories =
+                  (totalFats * 9 + totalCarbs * 4 + totalProteins * 4)
                 });
               }
               return (
